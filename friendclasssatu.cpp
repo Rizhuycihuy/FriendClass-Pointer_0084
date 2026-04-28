a69a909ea5396c6b15a2dfba726c06d62e0c52ee
Rizhuycihuy <rizki003131@gmail.com>
2026-04-28 15:35:21 +0700

    add void siswa,orang,siswa dan int main

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -6,19 +6,43 @@ class siswa;
 class orang
 {
   private:
-          string nama;
+          string Nama;
 
   public:
-          void setnama (string pnama);
+          void setNama (string pNama);
           friend class siswa;
 };
 
 class siswa
 {
   private:
-          int id;
+          int Id;
   
   public:
-          void setid(int pid);
-          void  displyall(orang &a);
+          void setId(int pId);
+          void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+  cout << Id << endl  << a.Nama;
+}
+
+void orang::setNama(string pNama)
+{
+  Nama = pNama;
+}
+void siswa::setId(int pId)
+{
+  Id = pId;
+}
+
+int main()
+{
+  orang joko;
+  joko.setNama("joko susilo");
+  siswa joko_siswa;
+  joko_siswa.setId(1);
+  joko_siswa.displayAll(joko);
+  return 0;
+}
