8a60e540149dd11b0ac4f7013678c8eb1dd0500a
Rizhuycihuy <rizki003131@gmail.com>
2026-04-28 15:23:07 +0700

    add class orang

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -2,3 +2,14 @@
 #include <string>
 using namespace std;
 
+class siswa;
+class orang
+{
+  private:
+          string nama;
+
+  public:
+          void setnama (string pnama);
+          friend class siswa;
+};
+
