9dd31d105230552ce7e6ccb6cc6b41adcfab059a
Rizhuycihuy <rizki003131@gmail.com>
2026-04-28 15:24:41 +0700

    add class siswa

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -13,3 +13,12 @@ class orang
           friend class siswa;
 };
 
+class siswa
+{
+  private:
+          int id;
+  
+  public:
+          void setid(int pid);
+          void  displyall(orang &a);
+};
